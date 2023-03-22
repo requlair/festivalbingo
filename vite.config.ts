37638a6ccc751840cada1578300b07c718d9f319
Lejon Bergman <lejonbergman@hotmail.com>
2023-03-22 15:48:21 +0100

    modified vite config

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import { VitePWA } from 'vite-plugin-pwa'
 // https://vitejs.dev/config/
 export default defineConfig({
   server: { host: true },
+  base: '/festivalbingo/',
   plugins: [
     vue(), 
     VitePWA({
