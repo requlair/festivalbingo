a550398695e2d8402aed4bf3a6a9b0387cbae8cc
Lejon Bergman <lejonbergman@hotmail.com>
2023-03-23 13:24:28 +0100

    changed background into components

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,6 @@
 <template>
-      <div :style="{ backgroundImage: `url(${backgroundImg})`}" class="fixed opacity-20 w-screen h-screen bg-[length:350px_350px] bg-no-repeat bg-center"></div>
+      <!-- <div :style="{ backgroundImage: `url(${backgroundImg})`}" class="fixed opacity-20 w-screen h-screen bg-[length:350px_350px] bg-no-repeat bg-center"></div> -->
+      <component :is="backgroundImg" class="fixed opacity-20 w-screen h-screen"></component>
       <div class="relative">
         <header class="p-2 flex justify-center">
           <div class="max-w-2xl">
@@ -15,16 +16,21 @@ import { storeToRefs } from 'pinia';
 import { ref, watchEffect } from 'vue';
 import BingoGame from './components/bingo/BingoGame.vue';
 import { useSettingsStore } from './stores/useSettingsStore';
+import BingoLogo from './assets/logos/bingo-logo.vue';
+import PaaspopLogo from './assets/logos/paaspop-logo.vue';
+import LowlandsLogo from './assets/logos/lowlands-logo.vue';
 
 const { getSettings } = storeToRefs(useSettingsStore());
 
 const headerImg = ref('default');
-const backgroundImg = ref('default');
+const backgroundImg = ref(BingoLogo);
 
 watchEffect(() => {
   document.querySelectorAll('body')[0].style.fontFamily = getSettings.value.fontFamily;
   import(/* @vite-ignore */ `./assets/images/${getSettings.value.headerImage}.png`).then((img) => headerImg.value = img.default);
-  import(/* @vite-ignore */ `./assets/images/${getSettings.value.backgroundImage}.svg`).then((img) => backgroundImg.value = img.default);
+  if (getSettings.value.backgroundImage === 'FB_Background') backgroundImg.value = BingoLogo;
+  if (getSettings.value.backgroundImage === 'PP_Background') backgroundImg.value = PaaspopLogo;
+  if (getSettings.value.backgroundImage === 'LL_Background') backgroundImg.value = LowlandsLogo;
 })
 </script>
 
