d1dfc782329e7b7bb10469818d9d9ae7917266a0
Lejon Bergman <lejonbergman@hotmail.com>
2023-03-30 12:38:00 +0200

    Updates

--- registerSW.js
+++ registerSW.js
@@ -1,1 +1,1 @@
-if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/festivalbingo/sw.js', { scope: '/festivalbingo/' })})}
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/sw.js', { scope: '/' })})}
